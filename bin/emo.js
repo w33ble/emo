b46342eb3ff2b73a36734963339a9f6d241a449d
Joe Fleming <joe.fleming@gmail.com>
2014-09-10 13:29:48 -0700

    add commander, allow multiple emoticons and verbose data

--- bin/emo.js
+++ bin/emo.js
@@ -1,10 +1,29 @@
 #!/usr/bin/env node
 
-var selector = require('../lib/selector');
-var tag = process.argv[2];
+var _ = require('lodash');
+var pkg = require('../package.json');
+var commander = require('commander');
+var emotes = require('../lib/emotes');
 
-// parse input params
-var emoticon = selector(tag);
+commander
+.version(pkg.version)
+.option('-c, --count <n>', 'specify number to return', parseInt)
+.option('-v, --verbose', 'get verbose info about the selected emoticon(s)')
+.parse(process.argv);
+
+var tag = commander.args.join(' ');
+var count = commander.count || 1;
+var emoticons = [];
+
+for (var i = 0; i < count; i++) {
+  emoticons.push(emotes.select(tag));
+}
+
+if (!commander.verbose) {
+  emoticons = _.map(emoticons, 'string');
+} else {
+  emoticons = _.map(emoticons, JSON.stringify);
+}
 
 // output selection
-console.log(emoticon.string);
+console.log(emoticons.join("\n"));
