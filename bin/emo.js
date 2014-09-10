ed0190d42030213a590b7c1da2c0cb41a3ba65f5
Joe Fleming <joe.fleming@gmail.com>
2014-09-10 16:23:02 -0700

    add -t option to list tags

--- bin/emo.js
+++ bin/emo.js
@@ -2,24 +2,41 @@
 
 var _ = require('lodash');
 var pkg = require('../package.json');
-var commander = require('commander');
+var cmd = require('commander');
+var table = require('text-table');
 var emotes = require('../lib/emotes');
 
-commander
+cmd
 .version(pkg.version)
+.option('-t, --tags', 'list available tags')
 .option('-c, --count <n>', 'specify number to return', parseInt)
 .option('-v, --verbose', 'get verbose info about the selected emoticon(s)')
 .parse(process.argv);
 
-var tag = commander.args.join(' ');
-var count = commander.count || 1;
+if (cmd.tags) {
+  var tags = emotes.getTags();
+  var tagList = [];
+  var limit = 5;
+
+  for (var i = 0; i < Math.ceil(tags.length / limit); i++) {
+    var start = i * limit;
+    var end = (i + 1) * limit;
+
+    tagList.push(tags.slice(start, end));
+  }
+  console.log(table(tagList));
+  return;
+}
+
+var tag = cmd.args.join(' ');
+var count = cmd.count || 1;
 var emoticons = [];
 
 for (var i = 0; i < count; i++) {
   emoticons.push(emotes.select(tag));
 }
 
-if (!commander.verbose) {
+if (!cmd.verbose) {
   emoticons = _.map(emoticons, 'string');
 } else {
   emoticons = _.map(emoticons, JSON.stringify);
