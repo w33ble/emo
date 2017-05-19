2f06e0b8983c43ce188b2b9abf6a88d6e1d69965
Joe Fleming <joe.fleming@gmail.com>
2017-05-18 18:02:16 -0700

    remove tag select option
    nothing happens with it now that inquirer is gone

--- bin/emo.js
+++ bin/emo.js
@@ -9,7 +9,6 @@ cmd
 .version(pkg.version)
 .usage('[options] [tag]')
 .option('-t, --tags', 'list available tags')
-.option('-s, --select', 'select emoticon tag from a list')
 .option('-c, --count <n>', 'specify number to return', parseInt)
 .option('-p, --clip', 'copy emoticon(s) to the clipboard')
 .option('-v, --verbose', 'get verbose info about the selected emoticon(s)')
